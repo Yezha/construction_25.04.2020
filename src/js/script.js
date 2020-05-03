7fe1c16004f7fef5d32c0cee8f97f7b26454bc67
Yerzhan <Hancogk3333@gmail.com>
2020-05-04 00:50:38 +0400

    сделал меню-бургер

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,4 @@
-function ibg(){
-
-  let ibg=document.querySelectorAll(".ibg");
-  for (var i = 0; i < ibg.length; i++) {
-  if(ibg[i].querySelector('img')){
-  ibg[i].style.backgroundImage = 'url('+ibg[i].querySelector('img').getAttribute('src')+')';
-  }
-  }
-  }
-  
-  ibg();
-
-  
-function ibg(){
-
-  $.each($('.ibg'), function(index, val) {
-  if($(this).find('img').length>0){
-  $(this).css('background-image','url("'+$(this).find('img').attr('src')+'")');
-  }
-  });
-  }
-  
-  ibg();
+$('.menu-btn').on('click', function(e){
+  e.preventDefault;
+  $(this).toggleClass('menu-btn_active')
+});
